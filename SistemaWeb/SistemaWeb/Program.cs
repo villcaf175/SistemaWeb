827bf89d1207eeefc83fa3de386d404a82a7627e
wilfredo <villcaf175@gmail.com>
2024-06-30 12:20:43 -0400

    CRUD login

--- SistemaWeb/SistemaWeb/Program.cs
+++ SistemaWeb/SistemaWeb/Program.cs
@@ -38,6 +38,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Login}/{action=Index}/{id?}");
 
 app.Run();
--- SistemaWeb/SistemaWeb/obj/Debug/net8.0/SistemaWeb.AssemblyInfo.cs
+++ SistemaWeb/SistemaWeb/obj/Debug/net8.0/SistemaWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdaf04d7b8a7910491c33286e26db2c3d0057da4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2684dd63d5bdbc012b28fdc0e3133402444807e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
